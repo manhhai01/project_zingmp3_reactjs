9f65308ed09f99d590a77a11cdfc2eb72e4b19a4
La Manh Hai <lamanhhai@Las-MacBook-Pro.local>
2023-07-13 12:00:52 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  // BrowserRouter,
+  unstable_HistoryRouter as HistoryRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App() {
+// custom history
+import { createBrowserHistory } from "history";
+import HomePage from "./pages/HomePage";
+
+export const customNavigate = createBrowserHistory();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HistoryRouter history={customNavigate}>
+      <Routes>
+        <Route path="home" element={<HomePage />}></Route>
+      </Routes>
+    </HistoryRouter>
   );
-}
+};
 
 export default App;
